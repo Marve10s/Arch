95f21faec1f3b629a3d83cacc26b8248e0c4fd3d
marve10s <igrimanigroman@gmail.com>
2024-03-04 21:12:38 +0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import "../styles/modern-normal.css";
+import "../styles/style.css";
+import "../styles/components/header.css";
+import "../styles/components/hero.css";
+import "../styles/components/about.css";
+import "../styles/components/features.css";
+import "../styles/components/projects.css";
+import "../styles/components/contact.css";
+import "../styles/components/footer.css";
+import "../styles/utility.css";
